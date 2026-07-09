4b48e9f6314fd961044fd085b3a57b16e7dffb67
Vidhikbk <vidhikhedkar.kbk@gmail.com>
2026-07-09 10:48:57 +0530

    add

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173", // React app
+        origin: ["http://localhost:5173", "http://localhost:5174"],
         credentials: true,
     })
 );
